fa4c7883d68bbd3bdf59d3422a72cb704f535125
danil <shuvalov58@icloud.com>
2023-11-28 18:42:37 +0300

    new homework

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY ,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100)NOT NULL,
+	title varchar(100) NOT NULL, 
+	birth_date date,
+	notes text NOT NULL
+);
+
+SELECT * FROM employees
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id SERIAL PRIMARY KEY,
+	customer_id_ VARCHAR(10) NOT NULL REFERENCES customers(customer_id),
+ 	employee_id_ INT NOT NULL REFERENCES employees(employee_id),
+	order_date DATE NOT NULL,
+    ship_city VARCHAR(30) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,58 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+with open('north_data/customers_data.csv', encoding='utf-8') as file:
+    file_reader = csv.reader(file, delimiter=',')
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='QWERasdf0058*')
+    try:
+        with conn:
+            with conn.cursor() as curs:
+                counter = 0
+                for element in file_reader:
+                    if counter == 0:
+                        counter += 1
+                        continue
+                    else:
+                        curs.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                                     (element[0], element[1], element[2]))
+                        counter += 1
+    finally:
+        conn.close()
+
+with open('north_data/employees_data.csv', encoding='utf-8') as file:
+    file_reader = csv.reader(file, delimiter=',')
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='QWERasdf0058*')
+    try:
+        with conn:
+            with conn.cursor() as curs:
+                counter = 0
+                for element in file_reader:
+                    if counter == 0:
+                        counter += 1
+                        continue
+                    else:
+                        curs.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                                     (element[0], element[1], element[2], element[3], element[4], element[5]))
+                        counter += 1
+    finally:
+        conn.close()
+
+with open('north_data/orders_data.csv', encoding='utf-8') as file:
+    file_reader = csv.reader(file, delimiter=',')
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='QWERasdf0058*')
+    try:
+        with conn:
+            with conn.cursor() as curs:
+                counter = 0
+                for element in file_reader:
+                    if counter == 0:
+                        counter += 1
+                        continue
+                    else:
+                        curs.execute("INSERT INTO  orders VALUES (%s, %s, %s, %s, %s)",
+                                     (element[0], element[1], element[2], element[3], element[4]))
+                        counter += 1
+    finally:
+        conn.close()
+
